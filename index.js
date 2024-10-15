f19baf7364a633d170e5497b308ed33bbc5c4ba6
tranminhhoan <hoanghhh098@gmail.com>
2024-10-15 17:10:11 +0700

    co ban

---
+++ index.js
@@ -0,0 +1,50 @@
+/*console.log(x,y,z)
+let x=1  pho bien rong rai
+var y=1 co dac tinh hoisting 
+const z=1 gia tri khong doi sau khi khoi tao vi no la hang so   */
+
+/*let x=10
+console.log(typeof x) // typeof thay the cho kieu du lieu (number)
+
+x="Hello"
+console.log(typeof x) // day la kieu du lieu String
+
+x= true
+console.log(typeof x) // boolean
+
+x = null 
+console.log(typeof x)// NULL
+
+x = undefined
+console.log(typeof x)// Undefined
+
+// Operator
+
+let y=0
+y += 2
+console.log(x) // 2
+
+u = "Hello"
+i = "World"
+console.log(u+i ) // co the dung toan tu de noi hai chuoi ki tu
+
+let h=100
+let g=99
+console.log(g<h)// true
+
+const e= true
+const r=false
+console.log(e && r)*/
+
+x= "Toi dang hoc code JavaScript"
+console.log(x)
+
+console.log("Hello, World")
+{
+    let x=1;
+    console.log(x)
+    
+
+}
+
+
