58fd0c542633787b154a38da836ae99c1ee6a405
tranminhhoan <hoanghhh098@gmail.com>
2024-10-17 23:11:44 +0700

    english

---
+++ index3.js
@@ -0,0 +1,25 @@
+/* các câu lệnh
+statements statements statements statement statement statements statement statements statements statements
+statement statement statement statement statement statement statement 
+Độc lập
+
+standalone standalone standalone standalone standalone standalone standalone standalone standalone 
+ standalone standalone standalone  standalone standalone standalone standalone standalone 
+
+Nguyên tắc 
+principle principle principle principle principle principle principle principle 
+principle principle principle principle principle 
+
+tại chổ 
+ 
+on-premise on premise on-premise on-premise on-premise on-premise on-premise
+
+Điểm cuối 
+Endpoint endpoint endpoint endpoint endpoint endpoint endpoint endpoint endpoint endpoint 
+
+các câu lệnh : statement 
+đọc lập: standalone, standalone 
+nguyên tắc : principle , principle principle 
+tại chổ: on-premise 
+điểm cuối: endpoint
+*/
