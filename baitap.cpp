ce508c4f0df6656f53d36323a49101fad058abf3
tranminhhoan <hoanghhh098@gmail.com>
2024-10-29 19:35:48 +0700

    baitap

--- baitap.cpp
+++ baitap.cpp
@@ -53,7 +53,7 @@ int main(){
 	else printf("day so khong giam dan. ");
 	
 }
-*/
+
 #include<stdio.h>
 #define Maxa 50
 void docDL( const char *Filename,  int a[], int *n){
@@ -97,4 +97,47 @@ int main(){
 	printf("nhap vao x can tim: ");
 	scanf("%d", &x);
 	printf("so lan ma x xuat hien la: %d", dem(a,n,x));
+}
+*/
+#include<stdio.h>
+double sum(int n) {
+	// Điều kiện dừng
+	if (n == 1) {
+		return 1.0; // Tổng của 1
+	} else {
+		// Tổng S(n) = 1/n + S(n-1)
+		return 1.0 / n + sum(n - 1);
+	}
+}
+long long Tich(int n){
+	if(n==1){
+		return 1;
+	}else{
+		return (2*n+1) * Tich(n-1);
+	}
+}
+int TongHieu(int n){
+	if(n==1){
+		return 1;
+	}else
+		return TongHieu(n-1) + ((n % 2==0)? -n: n);
+	
+}
+
+int main() {
+	int n;
+	printf("nhap n: ");
+	scanf("%d", &n);
+	
+	if (n <= 0) {
+		printf("Vui long nhap n > 0.\n");
+		return 1;
+	}
+	
+	int result = TongHieu(n);// Tong(n); Tich(n);
+//	printf("Tong S = 1 + 1/2 + ... + 1/%d = %.6f\n", n, result);
+//	printf("Tich S = 1 * 3 * 5 * ... *(2* %d + 1)= %lld\n",n,result);
+	printf("TongHieu S= 1-2+3-4+...+((-1)^%d+1)*%d= %d\n ", n,n,result);
+	
+	return 0;
 }
